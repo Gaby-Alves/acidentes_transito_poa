c5c3331b747f7b5aa3239c52f4c458a345b25107
Gaby-Alves <gabrielaalmeida97@gmail.com>
2021-09-27 23:18:04 -0300

    Transformando as datas em data

--- lendo_json.R
+++ lendo_json.R
@@ -1,9 +1,15 @@
-url <- "https://dadosabertos.poa.br/api/3/action/datastore_search?resource_id=42f022be-2723-4502-8a9a-05b282bf3ac5"
-
-
 library(jsonlite)
-library(dplyr)
+library(tidyverse)
+
+url <- "https://dadosabertos.poa.br/api/3/action/datastore_search?resource_id=b56f8123-716a-4893-9348-23945f1ea1b9&limit=500000"
+
 
 teste2 <- jsonlite::fromJSON(url) 
 
 teste2$result$records %>% as.data.frame -> df
+
+str(df)
+
+df$data_extracao <- as.Date(df$data_extracao)
+df$data <- as.Date((df$data))
+glimpse(df)
