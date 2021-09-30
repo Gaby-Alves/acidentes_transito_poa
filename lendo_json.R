3e98ca7fd5b20990ed69b89620cfbbb23daeaddd
Gaby-Alves <gabrielaalmeida97@gmail.com>
2021-09-29 21:20:02 -0300

    transformando alguns factors

--- lendo_json.R
+++ lendo_json.R
@@ -36,12 +36,25 @@ df$longitude <- as.character(df$longitude)
 # arrumando o numero do logradouro
 df$predial1 <- as.character(df$predial1)
 
+# transformando em factor
+# tipo_acident
+df$tipo_acid <- as.factor(df$tipo_acid)
+# dia da semana
+df$dia_sem <- as.factor(df$tipo_acid)
+# noite_dia
+df$noite_dia <- as.factor(df$noite_dia)
+#  regiao
+df$regiao <- as.factor(df$regiao)
+# consorcio
+df$consorcio  <- as.factor(df$consorcio)
+
+# conferindo o dataset arrumado
+glimpse(df)
 # Conhecendo a base-------------------------------------------------------------
-# Dimensão
+# Dimensão do data set ---------------------------------------------------------
 cat("O dataset contém", nrow(df)," linhas e", ncol(df)," colunas.")
 
-
-
+# Criando um for para descrever as colunas do dataset.
 descricao <- table(sapply(df,class))
 descricao[1]
 type(descricao)
@@ -50,6 +63,14 @@ for(i in seq_along(descricao)) {
   cat("Há ", descricao[i], "colunas do tipo ", names(descricao)[i],"\n")
 }
 
+# Verificando todo o data set
 glimpse(df)
 
+# Conhecendo um resumo dos dados.
+summary(df)
+# Links utilizados para essa sessão.
+#https://analisereal.com/2016/02/27/loops-no-r-usando-o-for-2/
+#https://stackoverflow.com/questions/2470248/write-lines-of-text-to-a-file-in-r
+#https://stackoverflow.com/questions/37702222/how-to-count-no-of-data-types-in-a-data-frame/37702266
 
+# 
