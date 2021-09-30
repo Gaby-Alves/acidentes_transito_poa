873737dffdfea0ab43f30bb80befdd36a80786ae
Gaby-Alves <gabrielaalmeida97@gmail.com>
2021-09-29 21:42:43 -0300

    Atualizando fatores e começando a visualizar os dados

--- script.R
+++ script.R
@@ -40,7 +40,7 @@ df$predial1 <- as.character(df$predial1)
 # tipo_acident
 df$tipo_acid <- as.factor(df$tipo_acid)
 # dia da semana
-df$dia_sem <- as.factor(df$tipo_acid)
+df$dia_sem <- as.factor(df$dia_sem)
 # noite_dia
 df$noite_dia <- as.factor(df$noite_dia)
 #  regiao
@@ -51,13 +51,11 @@ df$consorcio  <- as.factor(df$consorcio)
 # conferindo o dataset arrumado
 glimpse(df)
 # Conhecendo a base-------------------------------------------------------------
-# Dimensão do data set ---------------------------------------------------------
+# Dimensão do data set 
 cat("O dataset contém", nrow(df)," linhas e", ncol(df)," colunas.")
 
 # Criando um for para descrever as colunas do dataset.
 descricao <- table(sapply(df,class))
-descricao[1]
-type(descricao)
 names(descricao)
 for(i in seq_along(descricao)) {
   cat("Há ", descricao[i], "colunas do tipo ", names(descricao)[i],"\n")
@@ -66,11 +64,25 @@ for(i in seq_along(descricao)) {
 # Verificando todo o data set
 glimpse(df)
 
-# Conhecendo um resumo dos dados.
-summary(df)
 # Links utilizados para essa sessão.
 #https://analisereal.com/2016/02/27/loops-no-r-usando-o-for-2/
 #https://stackoverflow.com/questions/2470248/write-lines-of-text-to-a-file-in-r
 #https://stackoverflow.com/questions/37702222/how-to-count-no-of-data-types-in-a-data-frame/37702266
 
-# 
+
+# Especificação do problema de pesquisa ----------------------------------------
+cat("O propósito deste projeto é a criação de um modelo capaz de prever a quantidade
+    de feridos por acidente de trânsito utilizando os dados de mobilidade da cidade
+    de Porto Alegre.")
+
+# Conhecendo um resumo dos dados.
+summary(df) 
+
+hist_feridos <- ggplot(df,
+                       aes(x=feridos))+
+                      geom_histogram(bins = 20, fill = "#440154FF")+
+                      ylab("Contagem")+
+                      xlab("Feridos")+
+                      theme_bw()
+hist_feridos
+
